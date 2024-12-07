5243e08e1744e3174f348c58030a055f3d2fd7e5
DBasic-cmd <oekisola589@gmail.com>
2024-12-07 09:39:19 +0100

    adjusted daisyui configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from "daisyui"
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
@@ -15,7 +16,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('daisyui'),
+    daisyui,
   ],
 }
 
